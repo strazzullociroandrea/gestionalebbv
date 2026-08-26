64d92c812fb7f2e356ef78cca7feff0bb3da1d32
ciro <ciroandreastrazzullo@icloud.com>
2026-08-27 01:21:46 +0200

    fix: set runtime to edge for LoginCard component

--- src/app/authenticate/(index)/page.tsx
+++ src/app/authenticate/(index)/page.tsx
@@ -18,6 +18,8 @@ import {useRouter} from "next/navigation";
 import {Label} from "@/components/ui/label";
 import {Button} from "@/components/ui/button";
 
+export const runtime = "edge";
+
 export default function LoginCard() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
