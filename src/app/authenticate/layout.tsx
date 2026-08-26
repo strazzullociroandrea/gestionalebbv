6e617448c37a7a98ee80922671c3832a4ad17537
ciro <ciroandreastrazzullo@icloud.com>
2026-08-27 01:46:49 +0200

    fix: set runtime to edge for layout component

--- src/app/authenticate/layout.tsx
+++ src/app/authenticate/layout.tsx
@@ -1,7 +1,7 @@
 import {getServerSession} from "@/lib/auth";
 import {Suspense} from 'react';
 import {redirect} from "next/navigation";
-
+export const runtime = 'edge'
 
 export default async function AuthenticateLayout({
                                                      children,
