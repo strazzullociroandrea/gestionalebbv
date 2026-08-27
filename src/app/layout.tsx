01d3d97e4d52a6b875ff6070d2f3549a41a3856b
ciro <ciroandreastrazzullo@icloud.com>
2026-08-27 12:45:31 +0200

    fix: set dynamic runtime and fetch cache for layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type {Metadata} from "next";
 import "./globals.css";
 import {TRPCReactProvider} from "@/trpc/react";
 import { TooltipProvider } from "@/components/ui/tooltip"
-
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 
 export const metadata: Metadata = {
     title: "Black Bulls Volley | Gestionale",
