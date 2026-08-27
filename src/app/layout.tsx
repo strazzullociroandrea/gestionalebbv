5c28294716c54a187d68af44404d75079c8969ba
ciro <ciroandreastrazzullo@icloud.com>
2026-08-27 20:08:39 +0200

    chore: update layout metadata for Black Bulls Volley platform

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import {TRPCReactProvider} from "@/trpc/react";
-import { TooltipProvider } from "@/components/ui/tooltip"
+import {TooltipProvider} from "@/components/ui/tooltip"
+
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 export const runtime = "edge";
 
 export const metadata: Metadata = {
     title: "Black Bulls Volley | Gestionale",
-    description: "Gestionale sportivo Black Bulls Volley",
-    authors: [{name: "Ciro A. Strazzullo", url: "https://cirostrazzullo.it"}]
+    description: "Piattaforma per la gestione della squadra di pallavolo Black Bulls Volley",
+    authors: [{name: "Ciro A. Strazzullo", url: "https://cirostrazzullo.it"}],
 };
 
 export default function RootLayout({
@@ -24,7 +25,7 @@ export default function RootLayout({
         <body>
         <TRPCReactProvider>
             <TooltipProvider>
-                        {children}
+                {children}
             </TooltipProvider>
         </TRPCReactProvider>
         </body>
